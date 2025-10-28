cb217d18e8573e9e2441bbf2450d8c13cfc42d22
DaniilIvanchin <103023595+DaniilIvanchin@users.noreply.github.com>
2025-10-28 15:37:28 +0500

    test12

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,8 @@ from services.auth.auth_service import AuthService
 from services.auth.models.register_request import RegisterRequest
 from services.auth.models.login_request import LoginRequest
 from services.university.university_service import UniversityService
-
+import time
+import requests
 
 faker = Faker()
 
@@ -67,3 +68,18 @@ def university_api_utils_admin(access_token):
 @pytest.fixture(scope="session")
 def university_service(university_api_utils_anonym):
     return UniversityService(university_api_utils_anonym)
+
+@pytest.fixture(scope="session", autouse=True)
+def wait_for_readiness():
+    timeout = 180
+    start_time = time.time()
+
+    while time.time() < start_time + timeout:
+        try:
+            response = requests.get(AuthService.SERVICE_URL+"/docs")
+            response.raise_for_status()
+            break
+        except Exception:
+            time.sleep(1)
+    else:
+        raise RuntimeError(f"Auth service wasn't started during '{timeout}' seconds.")
